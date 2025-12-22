6ffd38c80ebc6af98350a15a204fef994ab8be3a
shi0001-99 <2198876433@qq.com>
2025-12-22 22:00:53 +0800

    add

--- main.cpp
+++ main.cpp
@@ -138,6 +138,125 @@ int main() {
     return 0;
 }
 
+
+//关于字符串，数组名与指针的辨析
+
+//输入多个名字
+//1.用二维数组
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    char name[5][10];  //可以定义一个二维数组，存储输入的多个字符串
+    for (int i = 0; i < 5; i++) {
+        cin >> name[i];  //name[i]是这个字符串的名字（类似于指针）
+    }
+    for (int i = 0; i < 5; i++) {
+        cout << name[i];
+    }
+    return 0;
+}
+//2.用cpp里面的string 
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string names[5];
+    for (int i = 0; i < 5; i++) {
+        cin >> names[i];
+    }    
+    for (int i = 0; i < 5; i++) {
+        cout << names[i] << " ";
+    }
+    return 0;
+}
+//3.用指针数组（麻烦）
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    char* name[5]；    
+    for (int i = 0; i < 5; i++) {
+        char* a = new char[10];//每次得新开辟一块空间，否则都指向一个字符数组char temp[10];
+        cin >> a;
+        name[i] = a;
+    }    
+    for (int i = 0; i < 5; i++) {
+        cout << name[i] << " ";
+        delete[] name[i];  // 记得释放内存
+    }
+    return 0;
+}
+
+//一维数组的数组名和指针的辨析
+#include <stdio.h>
+int main() {
+    char name[10] = "Hello";
+    
+    printf("name     = %p\n", (void*)name);      // 指向第一个字符的指针
+    printf("&name[0] = %p\n", (void*)&name[0]);  // 指向第一个字符的指针 类型是char*
+
+    printf("&name    = %p\n", (void*)&name);     // 指向整个数组的指针 类型是char(*)[10]  (二维数组数组名)
+
+    return 0;
+}
+//二维数组的数组名和指针的辨析
+char arr[3][4]={......};
+printf("arr         = %p\n", (void*)arr);          // 指向第一行（第一个一维数组）的指针
+printf("&arr[0]     = %p\n", (void*)&arr[0]);      // 同上(类型是char(*)[4])
+
+printf("arr[0]      = %p\n", (void*)arr[0]);       // 指向第一个元素的指针（相当于一维数组数组名）
+printf("&arr[0][0]  = %p\n", (void*)&arr[0][0]);   // 同上(类型是char*)
+
+printf("&arr        = %p\n", (void*)&arr);         // 指向整个二维数组的指针(类型是char(*)[3][4])
+
+
+
+//形参写法汇总
+
+// 1.
+char arr[3][4];
+func1(arr);
+// 形参写法1：指针数组
+void func1(char (*p)[4])  //char(*p)[4]是指向数组的指针，   char *p[4]是指针数组
+    
+// 形参写法2：数组形式
+void func1(char p[][4]) 
+
+
+
+// 2.
+char arr[3][4];
+func3(arr[0]);      // 传递第一行
+func3(arr[1]);      // 传递第二行
+func3(&arr[2][0]);  // 传递第三行，等价写法
+// 形参写法1：指针
+void func3(char *p)
+// 形参写法2：一维数组
+void func3(char p[]) 
+
+
+//3.
+char arr[3][4];
+func5(&arr);  // 传递整个数组的地址
+// 形参写法：指向整个二维数组的指针
+void func5(char (*p)[3][4]) 
+
+//4.
+char* arr[5];
+func1(arr, 5);
+void func1(char** p, int size) //指向指针的指针，所以是二级指针
+
+void func1(char* p[], int size)
+
+void func2(char* p[5]) //知道有几个单词
+
+func3(&arr)
+void func3(char* (*p)[5])
+
+
 //头部插入（倒序输入）
 #include<iostream>
 #include<string>
@@ -291,122 +410,6 @@ void DeleteLink(Node*head){
     }
 }
 
-//关于字符串，数组名与指针的辨析
-
-//输入多个名字
-//1.用二维数组
-#include<iostream>
-#include<string>
-using namespace std;
-
-int main() {
-    char name[5][10];  //可以定义一个二维数组，存储输入的多个字符串
-    for (int i = 0; i < 5; i++) {
-        cin >> name[i];  //name[i]是这个字符串的名字（类似于指针）
-    }
-    for (int i = 0; i < 5; i++) {
-        cout << name[i];
-    }
-    return 0;
-}
-//2.用cpp里面的string 
-#include<iostream>
-#include<string>
-using namespace std;
-
-int main() {
-    string names[5];
-    for (int i = 0; i < 5; i++) {
-        cin >> names[i];
-    }    
-    for (int i = 0; i < 5; i++) {
-        cout << names[i] << " ";
-    }
-    return 0;
-}
-//3.用指针数组（麻烦）
-#include<iostream>
-#include<string>
-using namespace std;
-int main() {
-    char* name[5]；    
-    for (int i = 0; i < 5; i++) {
-        char* a = new char[10];//每次得新开辟一块空间，否则都指向一个字符数组char temp[10];
-        cin >> a;
-        name[i] = a;
-    }    
-    for (int i = 0; i < 5; i++) {
-        cout << name[i] << " ";
-        delete[] name[i];  // 记得释放内存
-    }
-    return 0;
-}
-
-//一维数组的数组名和指针的辨析
-#include <stdio.h>
-int main() {
-    char name[10] = "Hello";
-    
-    printf("name     = %p\n", (void*)name);      // 指向第一个字符的指针
-    printf("&name[0] = %p\n", (void*)&name[0]);  // 指向第一个字符的指针 类型是char*
-
-    printf("&name    = %p\n", (void*)&name);     // 指向整个数组的指针 类型是char(*)[10]  (二维数组数组名)
-
-    return 0;
-}
-//二维数组的数组名和指针的辨析
-char arr[3][4]={......};
-printf("arr         = %p\n", (void*)arr);          // 指向第一行（第一个一维数组）的指针
-printf("&arr[0]     = %p\n", (void*)&arr[0]);      // 同上(类型是char(*)[4])
-
-printf("arr[0]      = %p\n", (void*)arr[0]);       // 指向第一个元素的指针（相当于一维数组数组名）
-printf("&arr[0][0]  = %p\n", (void*)&arr[0][0]);   // 同上(类型是char*)
-
-printf("&arr        = %p\n", (void*)&arr);         // 指向整个二维数组的指针(类型是char(*)[3][4])
-
-
-
-//形参写法汇总
-
-// 1.
-char arr[3][4];
-func1(arr);
-// 形参写法1：指针数组
-void func1(char (*p)[4])  //char(*p)[4]是指向数组的指针，   char *p[4]是指针数组
-    
-// 形参写法2：数组形式
-void func1(char p[][4]) 
-
-
-
-// 2.
-char arr[3][4];
-func3(arr[0]);      // 传递第一行
-func3(arr[1]);      // 传递第二行
-func3(&arr[2][0]);  // 传递第三行，等价写法
-// 形参写法1：指针
-void func3(char *p)
-// 形参写法2：一维数组
-void func3(char p[]) 
-
-
-//3.
-char arr[3][4];
-func5(&arr);  // 传递整个数组的地址
-// 形参写法：指向整个二维数组的指针
-void func5(char (*p)[3][4]) 
-
-//4.
-char* arr[5];
-func1(arr, 5);
-void func1(char** p, int size) //指向指针的指针，所以是二级指针
-
-void func1(char* p[], int size)
-
-void func2(char* p[5]) //知道有几个单词
-
-func3(&arr)
-void func3(char* (*p)[5])
 
 //链表输入的排序，交并差集的运算。
 #include <iostream>
